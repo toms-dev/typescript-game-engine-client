1a12d7e7ded739464f777d5ac3d4e4a63e02e492
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-20 04:30:11 +0100

    Added client library.

---
+++ lib/network/InternalMessageType.ts
@@ -0,0 +1,11 @@
+
+var InternalMessageType = {
+	HANDSHAKE: "internal_HANDSHAKE",
+	JOIN_GAME: "internal_JOIN_GAME",
+	INPUT_STATE: "internal_INPUT_STATE",
+	SERVER_STATE: "internal_SERVER_STATE",
+	PING_MEASURE: "internal_PING_MEASURE",
+	PING_VALUE: "internal_PING_VALUE"
+};
+
+export default InternalMessageType;
