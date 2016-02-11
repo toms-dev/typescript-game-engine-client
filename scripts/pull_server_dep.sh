c9b7e9d3d1beda4c5449ad05cecd55935f047659
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-02-11 20:58:34 +0100

    Cleaned build

---
+++ scripts/pull_server_dep.sh
@@ -0,0 +1,5 @@
+cd node_modules
+rm -rf typescript-game-engine-server
+git clone -b build-compiled https://github.com/toms-dev/typescript-game-engine-server.git
+cd typescript-game-engine-server
+npm install
